da8072e2735b75046dc1dc48b67067f623bdc59f
Raymond Zhao <raymondzhao@berkeley.edu>
2023-11-04 11:05:59 -0700

    refactor export, add __main__, add average+bipolar referencing, fix overwrite bug, implement folder export

--- edf_merge.py
+++ edf_merge.py
@@ -7,7 +7,9 @@ import scipy
 import pyedflib
 from pyedflib import highlevel
 import mne
-# common average referencing
+import sys
+
+
 """
 Script Usage:
     1) Move script into a folder.
@@ -39,29 +41,28 @@ def trim_and_decim(edf_path, freq):
     print_edf(data, 'Output')   
     return data
 
-def concat(raw_a, raw_b):
+def concat(lst):
     """Concatenates and returns two mne.io.Raw objects"""
-    return mne.concatenate_raws([raw_a, raw_b])
+    return mne.concatenate_raws(lst)
 
 def bipolar_reference(raw_edf):
-    channels = raw_edf.ch_names
     cathodes = ['Fp1-Ref', 'F7-Ref', 'T7-Ref', 'P7-Ref', 'Fp1-Ref', 'F3-Ref', 'C3-Ref', 'P3-Ref', 'Fz-Ref', 'Cz-Ref', 'Fp2-Ref', 'F4-Ref', 'C4-Ref', 'P4-Ref', 'Fp2-Ref', 'F8-Ref', 'T8-Ref', 'P8-Ref']
     anodes =   ['F7-Ref' , 'T7-Ref', 'P7-Ref', 'O1-Ref', 'F3-Ref',  'C3-Ref', 'P3-Ref', 'O1-Ref', 'Cz-Ref', 'Pz-Ref', 'F4-Ref',  'C4-Ref', 'P4-Ref', 'O2-Ref', 'F8-Ref',  'T8-Ref', 'P8-Ref', 'O2-Ref']
     names =    ['Fp1_F7',  'F7_T7',  'T7_P7',  'P7_O1',  'Fp1_F3',  'F3_C3',  'C3_P3',  'P3_O1',  'Fz_Cz',  'Cz_Pz',  'Fp2_F4',  'F4_C4',   'C4_P4', 'P4_O2',  'Fp2_F8',  'F8_T8',  'T8_P8',  'P8_O2']
-    return mne.set_bipolar_reference(raw_edf, anodes, cathodes, drop_refs = True)
+    return mne.set_bipolar_reference(raw_edf, anodes, cathodes, names)
 
 def average_reference(raw_edf):
     return raw_edf.set_eeg_reference()
 
-def export(raw_edf, target_name, bipolar=True, common_average=True, bipolar_common_average=True):
+def export(raw_edf, target_name, overwrite_existing=True, bipolar=False, common_average=False, bipolar_common_average=False):
     """Export raw object as EDF file"""
-    mne.export.export_raw(target_name+'.edf', raw_edf, overwrite=True)
+    mne.export.export_raw(target_name + '.edf', raw_edf, overwrite=True)
     if bipolar:
-        mne.export.export_raw(target_name + '-bipolar.edf', bipolar_reference(raw_edf), overwrite=True)
+        mne.export.export_raw(target_name + '-bipolar.edf', bipolar_reference(raw_edf), overwrite=overwrite_existing)
     if common_average:
-        mne.export.export_raw(target_name + '-common-average.edf', average_reference(raw_edf), overwrite=True)
+        mne.export.export_raw(target_name + '-common-average.edf', average_reference(raw_edf), overwrite=overwrite_existing)
     if bipolar_common_average:
-        mne.export.export_raw(target_name + '-bipolar-common-average.edf', average_reference(bipolar_reference(raw_edf)), overwrite=True)
+        mne.export.export_raw(target_name + '-bipolar-common-average.edf', average_reference(bipolar_reference(raw_edf)), overwrite=overwrite_existing)
 
 def print_edf(raw_edf, name):
     """Print basic information about an mne.io.raw object."""
@@ -70,26 +71,35 @@ def print_edf(raw_edf, name):
     print('Dim:', raw_edf.get_data().shape[0], 'channels', 'x', raw_edf.get_data().shape[1], 'time points\n\n\n')
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        name = 'output'
+    else:
+        name = sys.argv[1]
     src = os.getcwd()
     edf_path_in = src + '\\in'
     os.chdir(edf_path_in)
+    #for root, folders, files in os.walk(edf_path_in):
 
-    """
-    for root, folders, files in os.walk(edf_path_in):
-        for folder in folders:
-            os.chdir(folder)
-            path = os.path.join
-            lst_of_raws = list(map(lambda r: mne.io.read_raw_edf(edf_path)))
-            merged = mne.concatenate_raws(lst_of_raws)
-
-    """
-
+    #for root, folders, files in os.walk(edf_path_in):
+        # print(root) - C:\Users\raymo\ray\urap\edf-merger\in
+        # print(list(folders)) - []
+        # print(list(files)) - ['PR03_0010.edf', 'PR03_0011.edf']
     src_files = list(sorted(os.listdir(edf_path_in)))
-    a, b = src_files[0], src_files[1]
-    a, b = trim_and_decim(a, 200), trim_and_decim(b, 200)
-
-    merged = concat(a, b)
+    merged = concat([trim_and_decim(edf, 200) for edf in src_files])
+    os.chdir(src)
+    #mne.export.export_raw('test.edf', merged)
+    #export(merged, 'test')
+    new = src + '\\out'
+    os.mkdir(new)
+    os.chdir(new)
+    export(merged, name)
+    #src_files = list(sorted(os.listdir(edf_path_in)))
+    #a, b = src_files[0], src_files[1]
+    #print(a, b)
+    #a, b = trim_and_decim(a, 200), trim_and_decim(b, 200)
+"""
+    merged = concat([a, b])
     print_edf(merged, 'Merged')
 
     # export
-    export(merged, 'test')
+    export(merged, 'test')"""
